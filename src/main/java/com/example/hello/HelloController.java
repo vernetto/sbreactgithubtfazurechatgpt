a32462e0d745621bc3237bdfb5d7f58a5147543a
Pierluigi Vernetto <publicpierre@gmail.com>
2025-12-19 15:16:51 +0100

    version 1.0

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
   @GetMapping("/hello")
   public String hello() {
-    return "Hello from Spring Boot!";
+    return "Hello from Spring Boot, version 1.0!";
   }
 
   @GetMapping("/")
